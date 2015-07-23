dd03d9acf77e44b8cd889dfc3e328eb21f5dc15a
Jefferson <hsujahhu@herxun.co>
2015-07-23 12:41:01 +0800

    Initial Commit

---
+++ NeiooPlayground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NeiooPlayground
+//
+//  Created by hsujahhu on 2015/7/23.
+//  Copyright (c) 2015年 Herxun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
